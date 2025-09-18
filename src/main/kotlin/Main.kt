f8d5723ef3f29f4132188e28609fbda9210f1978
SF-Gueye <ultra-necrozma@hotmail.com>
2025-09-18 11:56:57 +0200

    V4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import org.example.dresseur.Entraineur
+import org.example.monstres.EspeceMonstre
 
 /**
  * Change la couleur du message donné selon le nom de la couleur spécifié.
@@ -31,13 +32,18 @@ fun changeCouleur(message: String, couleur:String=""): String {
 var joueur = Entraineur(1,"Sacha",100)
 var rival = Entraineur(2,"Regis",200)
 
+var espece_Springleaf = EspeceMonstre(1, "Springleaf", "Graine", 9, 11, 10, 12, 14, 60, 6.5, 9.0, 8.0, 7.0, 10.0, 34.0, "Petit monstre espiègle rond comme une graine, adore le soleil.", "Sa feuille sur la tête indique son humeur.", "Curieux, amical, timide")
+var espece_Flamkip = EspeceMonstre(4, "Flamkip", "Animal", 12, 8, 13, 16, 7, 50, 10.0, 5.5, 9.5, 9.5, 6.5, 22.0, "Petit animal entouré de flammes, déteste le froid.", "Sa flamme change d’intensité selon son énergie.", "Impulsif, joueur, loyal")
+var espece_Aquamy = EspeceMonstre(7, "Aquamy", "Meteo", 10, 11, 9, 14, 14, 55, 9.0, 10.0, 7.5, 12.0, 12.0, 27.0, "Créature vaporeuse semblable à un nuage, produit des gouttes pures.", "Fait baisser la température en s’endormant.", "Calme, rêveur, mystérieux")
+var espece_Laoumi = EspeceMonstre(8, "Laoumi", "Animal", 11, 10, 9, 8, 11, 58, 11.0, 8.0, 7.0, 6.0, 11.5, 23.0, "Petit ourson au pelage soyeux, aime se tenir debout.", "Son grognement est mignon mais il protège ses amis.", "Affectueux, protecteur, gourmand")
+var espece_Bugsyface = EspeceMonstre(10, "Bugsyface", "Insecte", 10, 13, 8, 7, 13, 45, 7.0, 11.0, 6.5, 8.0, 11.5, 21.0, "Insecte à carapace luisante, se déplace par bonds et vibre des antennes.", "Sa carapace devient plus dure après chaque mue.", "Travailleur, sociable, infatigable")
+var espece_Galum = EspeceMonstre(13, "Galum", "Mineral", 12, 15, 6, 8, 12, 55, 9.0, 13.0, 4.0, 6.5, 10.5, 13.0, "Golem ancien de pierre, yeux lumineux en garde.", "Peut rester immobile des heures comme une statue.", "Sérieux, stoïque, fiable")
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 fun main() {
-    joueur.afficheDetail()
-    rival.afficheDetail()
-    joueur.argents+=50
-    joueur.afficheDetail()
+    println(espece_Springleaf.afficheArt())
+    println(espece_Flamkip.afficheArt())
+    println(espece_Aquamy.afficheArt())
 }
--- src/main/kotlin/monstres/EspeceMonstre.kt
+++ src/main/kotlin/monstres/EspeceMonstre.kt
@@ -1,5 +1,7 @@
 package org.example.monstres
 
+import java.io.File
+
 class EspeceMonstre(
     var id : Int,
     var nom: String,
@@ -28,9 +30,9 @@ class EspeceMonstre(
      * @return Une chaîne de caractères contenant l'art ASCII du monstre avec les codes couleur ANSI.
      *         L'art est lu à partir d'un fichier texte dans le dossier resources/art.
      */
-    fun afficheArt(deFace: Boolean=true): String{
+    fun afficheArt(deFace: String =true): String{
         val nomFichier = if(deFace) "front" else "back";
-        val art=  File("src/main/resources/art/${this.nom.lowercase()}/$nomFichier.txt").readText()
+        val art= File("src/main/resources/art/${this.nom.lowercase()}/$nomFichier.txt").readText()
         val safeArt = art.replace("/", "∕")
         return safeArt.replace("\\u001B", "\u001B")
     }
